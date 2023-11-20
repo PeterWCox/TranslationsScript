b2744555335c162811a0901fb6d818fe09775349
Peter Cox <p.cox@outlook.com>
2023-11-20 17:27:38 +0000

    Working build

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
-// Import the object from the external file
-const inputObject = require("C:\\git\\TranslationsScript\\i18ndata\\en\\core.js"); // Update with the correct path
+// Extract the text from the file
+const fileText = fs.readFileSync(
+  path.resolve(__dirname, "C:/git/TranslationsScript/i18ndata/en/core.js"),
+  "utf8"
+);
+
+// console.log(fileText);
+
+// Use a regular expression to extract the object
+const objectRegex = /export default (\{[\s\S]*?\});/;
+
+// Match the regex in the input text
+const match = fileText.match(objectRegex);
+const extractedObjectString = match[1];
+const extractedObject = eval(`(${extractedObjectString})`);
 
 // Function to recursively sort object keys
 const sortObjectKeys = (obj) => {
@@ -25,19 +38,56 @@ const sortObjectKeys = (obj) => {
 };
 
 // Sort the object keys
-const sortedObject = sortObjectKeys(inputObject);
+const sortedObject = sortObjectKeys(extractedObject);
+
+//Remove the parenthesesis from the object keys
+const removeParenthesis = (obj) => {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(removeParenthesis);
+  }
+
+  const sortedObj = {};
+  Object.keys(obj)
+    .sort()
+    .forEach((key) => {
+      sortedObj[key.replace(/[\(\)]/g, "")] = removeParenthesis(obj[key]);
+    });
+
+  return sortedObj;
+};
+
+const finalObject = removeParenthesis(sortedObject);
+
+const { exec } = require("child_process");
 
 // Add the copyright comment and export line to the sorted object
 const finalOutput = `// Copyright (c) Leo RegTech Limited. All rights reserved.\n\nexport default ${JSON.stringify(
-  sortedObject,
+  finalObject,
   null,
   2
 )};`;
 
 // Define the output file path
-const outputPath = path.join(__dirname, "path", "to", "output", "file.js");
+const outputPath = "C:\\git\\TranslationsScript\\output.js";
 
 // Write the sorted object to a new file
 fs.writeFileSync(outputPath, finalOutput);
 
-console.log(`Sorted object has been saved to ${outputPath}`);
+const prettier = require("prettier");
+
+// Run Prettier command
+exec(`npx prettier --write ${outputPath}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Prettier output an error: ${stderr}`);
+    return;
+  }
+  console.log(`Prettier successfully formatted ${outputPath}`);
+});
---
+++ output.js
@@ -0,0 +1,50 @@
+// Copyright (c) Leo RegTech Limited. All rights reserved.
+
+export default {
+  date: {
+    offsetLabel: "How long after user registration?",
+    range: {
+      startAfterEnd: "{0} cannot be after {1}",
+    },
+    switch: {
+      exact: "Switch to exact date",
+      offset: "Switch to relative date",
+    },
+  },
+  group: {
+    apply: "Apply Grouping",
+    clear: "Clear Grouping",
+    groupDialog: "Group Data",
+    grouping: "Group by Columns",
+    help: "Group the data",
+    summary: "Summary Columns",
+    type: {
+      count: "Count of set values",
+      group: "Group by the value",
+      hidden: "Hide column",
+      sum: "Sum of numeric values",
+      unique: "Show unique values only",
+    },
+  },
+  richText: {
+    formatParagraph: "Paragraph format",
+    insertText: "Insert text",
+    placeHolder: "Please enter your text here",
+  },
+  schedule: {
+    firstInstance: "First Instance",
+    frequency: {
+      daily: "Daily",
+      weekly: "Weekly",
+    },
+    repeatingPattern: "Repeating Pattern",
+  },
+  search: {
+    results: 'Search results for "{0}"',
+  },
+  sort: {
+    asc: "Ascending",
+    click: "Click to sort",
+    desc: "Descending",
+  },
+};
